973e3262ed1a2a819eac0a4826cda0f268ad4b2c
happymaj00r <gameforlife20@hotmail.de>
2024-10-12 14:56:21 +0200

    Added Cache and menu fix

--- main.lua
+++ main.lua
@@ -6,6 +6,8 @@ end
 local patterns = { "^HarvestNode","Door", "Chest", "Clicky", "Cairn", "Break", "LooseStone", "Corpse", "Switch" }
 local menu = require("menu");
 
+local last_update_time = 0
+local update_interval = 0.1
 
 local function matchesAnyPattern(skin_name, extra)
     for _, pattern in ipairs(patterns) do
@@ -20,20 +22,29 @@ on_render_menu(function()
     if not menu.main_tree:push("Auto Interact") then
         return;
     end;
+	menu.main_boolean:render("Enable Plugin", "");
+	if menu.main_boolean:get() == false then
+      -- plugin not enabled, stop rendering menu elements
+      menu.main_tree:pop();
+      return;
+    end;
     menu.main_openContainers:render("Open Containers", "");
-
     menu.main_showContainers:render("Show Containers", "");
+	menu.main_tree:pop();
 
 end)
 
+
+
+local actors_cache = {}
 local Interact_Delay = 0.0;
 -- on_update callback
 on_update(function()
     local local_player = get_local_player();
 	
-    if not local_player then
+	if not local_player or menu.main_boolean:get() == false then
         return;
-    end
+    end;
 	if get_time_since_inject() < Interact_Delay then
         return;
     end;
@@ -43,8 +54,11 @@ on_update(function()
     end
     local playerPos = local_player:get_position();
     local objects = actors_manager.get_ally_actors()
-    -- clear table if too big    
-    local function shouldInteract(skin_name, position)
+       
+	actors_cache = {}
+	
+    local function shouldInteract(Obj, position)
+		skin_name = Obj:get_skin_name()
         local distanceThreshold = 1.5 -- Default
         if skin_name:match("Shrine") then
             distanceThreshold = 2.5
@@ -53,15 +67,15 @@ on_update(function()
         elseif not matchesAnyPattern(skin_name) then
             return false -- early break
         end
+		table.insert(actors_cache, {Object = Obj, position = position, skin_name = skin_name})
         return position:dist_to(playerPos) < distanceThreshold
     end
 
     for i, obj in ipairs(objects) do 
-        if obj:is_interactable() then
-			local obj_id = obj:get_id()
+        if obj:is_interactable() then			
             local position = obj:get_position();
             local skin_name = obj:get_skin_name()
-            if skin_name and shouldInteract(skin_name, position) then
+            if skin_name and shouldInteract(obj, position) then
                 interact_object(obj)
 				console.print("Interacting");
 				Interact_Delay = get_time_since_inject() + 0.2;
@@ -79,12 +93,12 @@ on_render(function()
     if not menu.main_showContainers:get() then
         return;
     end
-    local objects = actors_manager.get_ally_actors()
+    local objects = actors_cache
     for i, obj in ipairs(objects) do
-        local obj_id = obj:get_id()
-		if obj:is_interactable() then
-				local position = obj:get_position();
-				local skin_name = obj:get_skin_name()
+        
+		if obj.Object:is_interactable() then
+				local position = obj.position;
+				local skin_name = obj.skin_name
 				if skin_name and matchesAnyPattern(skin_name, true) then
 					graphics.circle_3d(position, 1, color_green(255));
 					graphics.text_3d("Open", position, 15, color_green(255));
--- menu.lua
+++ menu.lua
@@ -1,8 +1,9 @@
-local plugin_label = "BECCAS_ALAKAZAM_PLUGIN_"
+local plugin_label = "Auto_Interact_PLUGIN_"
 local menu_elements =
 {
+	main_boolean        = checkbox:new(true, get_hash(plugin_label .. "main_boolean")),
     main_openContainers      = checkbox:new(true, get_hash(plugin_label .. "main_openContainers")),
     main_showContainers = checkbox:new(true, get_hash(plugin_label .. "main_showContainers")),
     main_tree           = tree_node:new(0),
 }
-return menu_elements;
+return menu_elements
